93057ccfa8e59de255b04a7971eba6c77bd4b379
Harm Manders <harmmanders@gmail.com>
2022-11-30 23:29:09 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <pre class="text-left">
+    {{ testFunction.name }} = {{ testFunction.toString() }}
+  </pre>
+</template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ref } from "vue";
+
+const testFunction = () => {
+  console.log("This is a test function");
+  console.log("another line");
+};
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
